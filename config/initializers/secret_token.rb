18bc06dfed7a3bba7c8b8f359eb6c20a40ad82ef
max <mk@kernel-corp.com>
2014-09-18 12:42:29 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inotex::Application.config.secret_key_base = 'aab93feeaf30d0c86574d31db4a5f5701c09cd34beead8df4607ffdf40f38b50a43d9dadbe1d2f451ab23fd44ea3e3796c112ad96693f65b7551ebba0047b1b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Inotex::Application.config.session_store :cookie_store, key: '_inotex_session'
