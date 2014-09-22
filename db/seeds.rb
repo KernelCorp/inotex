56653dd7bcb59bf1d87342cf7d14baba0b396998
max <mk@kernel-corp.com>
2014-09-22 19:19:42 +0700

    index -> root

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+OrdinaryCms::Page.create name: 'root', root: true
