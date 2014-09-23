e9632efdde836af2496b55bd65ab2e11a3360d19
max <mk@kernel-corp.com>
2014-09-23 15:36:31 +0700

    for deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+AdminUser.create email: 'admin@example.com', password: 'password'
+
 OrdinaryCms::Page.create name: 'root', root: true
