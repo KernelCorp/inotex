8ff171396722865eda4e8cc4d6cf36df3a7f62de
mitchenkod <mitchenko1897@mail.ru>
2014-09-24 16:56:02 +0700

    to pull

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Inotex::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  # mount Mercury::Engine => '/'
+  mount Mercury::Engine => '/'
   mount OrdinaryCms::Engine => '/'
   ActiveAdmin.routes(self)
   resources :pages, only: :show
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,19 @@ AdminUser.create email: 'admin@example.com', password: 'password'
 
 OrdinaryCms::Page.create name: 'contacts'
 OrdinaryCms::Page.create name: 'root', root: true
+
+text_page = OrdinaryCms::Factories::Page.create name: 'text_page'
+
+text_page.sections.build(name: 'title')
+text_page.sections.build(name: 'first_text')
+text_page.sections.build(name: 'red_rect_text')
+text_page.sections.build(name: 'second_text')
+text_page.sections.build(name: 'top_title')
+text_page.sections.build(name: 'after_title')
+text_page.sections.build(name: 'small_title')
+text_page.save
+text_page.build(name: 'Инженерные системы').save
+text_page.build(name: 'Информационная безопасноть').save
+text_page.build(name: 'Создание web-проектов').save
+text_page.build(name: 'Инсталляция программного обеспеченния').save
+text_page.build(name: 'Поддержка 1С').save
