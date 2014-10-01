f8d0958546b796d1f9e0f8d22c2efca6d9874986
max <mk@kernel-corp.com>
2014-10-01 16:51:07 +0700

    refactoring

--- app/helpers/ordinary_cms/pages_helper.rb
+++ app/helpers/ordinary_cms/pages_helper.rb
@@ -1,5 +1,7 @@
-module OrdinaryCms::PagesHelper
-  def get_pages
-    OrdinaryCms::Factories::Page.where(name: 'text_page').first.pages.order('position asc')
+module OrdinaryCms
+  module PagesHelper
+    def get_pages
+      OrdinaryCms::Factories::Page.where(name: 'text_page').first.pages.order('position asc')
+    end
   end
 end
